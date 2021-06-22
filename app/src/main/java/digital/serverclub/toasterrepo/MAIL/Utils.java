53c77770fd7f8f760079b4cb0dc745372ae081b5
ServerClub <buddhika.chathuranga@serverclub.lk>
2021-06-22 18:26:55 +0530

    third

---
+++ app/src/main/java/digital/serverclub/toasterrepo/MAIL/Utils.java
@@ -0,0 +1,8 @@
+package digital.serverclub.toasterrepo.MAIL;
+
+public class Utils {
+    //This is your from email
+    public static final String EMAIL = "buddhika.chathuranga@serverclub.lk";
+    //This is your from email password
+    public static final String PASSWORD = "vvvvvvv";
+}
--- app/src/main/java/digital/serverclub/toasterrepo/MainActivity.java
+++ app/src/main/java/digital/serverclub/toasterrepo/MainActivity.java
@@ -4,12 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import digital.serverclub.toasterrepo.MAIL.JavaMailAPI;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+
+
+    public void sendMail(String email,String msg) {
+
+
+        //Send Mail
+        JavaMailAPI javaMailAPI = new JavaMailAPI(this,email,"Chathu pathirana testing mail server",msg);
+
+        javaMailAPI.execute();
 
     }
 }
